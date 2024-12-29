8d54b0942811617874fb21ec51424ce990dee961
kristywen <kristywen@lutlook.com.tr>
2024-12-29 23:20:34 +0300

    fix main

--- src/main/java/me/kristywen/test/Main.java
+++
@@ -1,8 +0,0 @@
-package me.kristywen.test;
-
-
-public class Main {
-    public static void main(String[] args) {
-        new Webhook().sendWebhook("https://discord.com/api/webhooks/1317535969913737339/yJA53qck3KDs8GEUrTihBvGEQhj4JIPWB25dX3pM6f50lvEy5o37HUpfsvZCJzNF3P3X","yo");
-    }
-}
